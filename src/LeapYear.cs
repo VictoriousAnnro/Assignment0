a207b63179c13d92df4aed0f65aef40e29ad63ca
VictoriousAnnro <112409949+VictoriousAnnro@users.noreply.github.com>
2022-09-02 19:19:28 +0200

    New b (#2)
    * testing git
    
    * changed stuff f u
    
    * added missing !!
    
    * added leapyear stuff
    
    * Tested and implemented IsLeapYear

---
+++ src/LeapYear.cs
@@ -0,0 +1,13 @@
+namespace src;
+
+
+public class LeapYear{
+
+    public bool isLeapYear(int year){
+        if(year % 4 == 0 && year % 100 != 0){
+            return true;
+        } else if (year % 400 == 0){
+            return true;
+        } else return false;
+    }
+}
