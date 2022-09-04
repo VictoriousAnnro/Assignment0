62dfa7ac35ae96ec933a01009ad8a13581ffcddd
Anne-Marie <annemarierommerdahl@gmail.com>
2022-09-04 15:37:53 +0200

    Created user interface for IsLeapYear2

--- src/LeapYear.cs
+++ src/LeapYear.cs
@@ -1,24 +1,8 @@
 namespace src;
 
 public class LeapYear{
-    static int yearInput;
 
-    public static void Main(string[] args){
-        Console.WriteLine("Please enter a year");
-
-        if(int.TryParse(Console.ReadLine(), out int J)){
-            yearInput = J;
-        } else yearInput = 0;
-        
-        if(isLeapYear(yearInput)){
-            Console.WriteLine("yay");
-        } else Console.WriteLine("nay");
-        
-    }
-
-    
-
-    public static bool isLeapYear(int year){
+    public bool isLeapYear(int year){
         if(year % 4 == 0 && year % 100 != 0){
             return true;
         } else if (year % 400 == 0){
